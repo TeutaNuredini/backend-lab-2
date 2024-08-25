7261c54822c40326dec4fbe827f39702e1f011ea
Qendresa Islami <qi47287@ubt-uni.net>
2024-08-25 13:49:40 +0200

    creating event repository

--- src/main/java/com/weppapp_be/teuta_qendresa/repository/EventRepository.java
+++ src/main/java/com/weppapp_be/teuta_qendresa/repository/EventRepository.java
@@ -2,6 +2,16 @@ package com.weppapp_be.teuta_qendresa.repository;
 
 import com.weppapp_be.teuta_qendresa.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT e FROM Event e WHERE e.id = :id AND e.deletedAt IS NULL")
+    Optional<Event> findById(Long id);
+
+    @Query("SELECT e FROM Event e WHERE e.deletedAt IS NULL")
+    List<Event> findAll();
 }
