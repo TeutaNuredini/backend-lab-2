18a52c50be2981f4144861bf5e4026eaebc30dbe
Qendresa Islami <qi47287@ubt-uni.net>
2024-08-25 13:48:01 +0200

    Creating Venue repository

--- src/main/java/com/weppapp_be/teuta_qendresa/repository/VenueRepository.java
+++ src/main/java/com/weppapp_be/teuta_qendresa/repository/VenueRepository.java
@@ -2,6 +2,16 @@ package com.weppapp_be.teuta_qendresa.repository;
 
 import com.weppapp_be.teuta_qendresa.entity.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface VenueRepository extends JpaRepository<Venue, Long> {
+
+    @Query("SELECT v FROM Venue v WHERE v.id = ?1 AND v.deletedAt IS NULL")
+    Optional<Venue> findById(Long id);
+
+    @Query("SELECT v FROM Venue v WHERE v.deletedAt IS NULL")
+    List<Venue> findAll();
 }
