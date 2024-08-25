4aa7e761297828e04faa8b276ba1ebcad580c298
Qendresa Islami <qi47287@ubt-uni.net>
2024-08-25 13:41:16 +0200

    Creating repository for reservation

---
+++ src/main/java/com/weppapp_be/teuta_qendresa/repository/ReservationRepository.java
@@ -0,0 +1,7 @@
+package com.weppapp_be.teuta_qendresa.repository;
+
+import com.weppapp_be.teuta_qendresa.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
