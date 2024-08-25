874316821a52759c8118bd3ab02c91b8899ec0a2
Qendresa Islami <qi47287@ubt-uni.net>
2024-08-25 13:48:46 +0200

    creating category repository

--- src/main/java/com/weppapp_be/teuta_qendresa/repository/CategoryRepository.java
+++ src/main/java/com/weppapp_be/teuta_qendresa/repository/CategoryRepository.java
@@ -3,6 +3,16 @@ package com.weppapp_be.teuta_qendresa.repository;
 
 import com.weppapp_be.teuta_qendresa.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("SELECT c FROM Category c WHERE c.id = ?1 AND c.deletedAt IS NULL")
+    Optional<Category> findById(Long id);
+
+    @Query("SELECT c FROM Category c WHERE c.deletedAt IS NULL")
+    List<Category> findAll();
 }
