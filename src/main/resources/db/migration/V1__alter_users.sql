bcfa2941ec55177ce99c897da19d6bb02deb5d64
Qendresa Islami <qi47287@ubt-uni.net>
2024-09-08 16:30:57 +0200

    fix bug

---
+++ src/main/resources/db/migration/V1__alter_users.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (created_at, updated_at, first_name, last_name, username, password, role)
+VALUES (
+           NOW(),
+           NOW(),
+           'Filan',
+           'Fisteku',
+           'admin',
+           '$2a$12$oLY6ZtGPL7TgyMou3cr.9egO0gv3p/QgxdAhoCcJ9YzOFQwDTKRIS',
+           'ADMIN'
+       )
